7be1e4c4ca9c46dbc0227a45e0775760d79585fa
mroda <mjroda30@gmail.com>
2024-11-07 19:47:06 +0800

    Feature Field

--- hr.py
+++ hr.py
@@ -1,2 +1,6 @@
 class VersionThree(Models.model):
     name = fields.Char("Name", store= True)
+    
+class FeatureOne(Models.model):
+    name = fields.Char("Name", store= True)
+    email = fields.Char("Email", store= True)
